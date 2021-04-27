1b65f90f9b180c59146a43f6754da17b1cda3a96
qingdannn <qingdannn@gmail.com>
2021-04-27 19:58:15 +0800

    整理

---
+++ client/app/src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155149902/util/UrlUtil.java
@@ -0,0 +1,8 @@
+package hk.edu.cuhk.ie.iems5722.a2_1155149902.util;
+
+public class UrlUtil {
+    public static final String TEST_URL = "http://10.0.2.2:5000"; //测试(测试环境)
+    public static final String REAL_URL = "http://18.219.150.95"; //正式服务器(生产环境)
+
+    public static String BaseUrl = REAL_URL;//默认为生产环境
+}
